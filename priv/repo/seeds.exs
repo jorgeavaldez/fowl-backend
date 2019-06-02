18245642a91ec525c31571e3169e4ccb37e2caa5
esposama <amandae@smu.edu>
2019-06-02 01:27:05 -0500

    update seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,15 +11,36 @@
 # and so on) as they will fail if something goes wrong.
 
 defmodule Seeds do
-  @seed_file File.read!("/Users/jorge/projects/fowl/priv/repo/players.json")
+  @seed_file File.read!("/Users/amandaesposito/projects/fowl/fowl-backend/priv/repo/players.json")
 
   def get_players_json do
     Jason.decode!(@seed_file)
   end
 end
 
-Seeds.get_players_json()["content"]
-|> Enum.each(fn player ->
+{:ok, last_player} = Seeds.get_players_json()["content"]
+|> Enum.reduce(fn player, _ ->
   %{blizzard_id: player["id"]}
   |> Fowl.OWL.create_players
 end)
+
+{:ok, user} = Fowl.Accounts.create_user(%{
+  email: "test@email.com",
+  password: "password"
+})
+
+{:ok, league} = Fowl.Leagues.create_league(%{
+  draft_time: "2016-09-13T23:30:52.123Z",
+  name: "the league"
+})
+
+{:ok, team} = Fowl.Leagues.create_team(%{
+  "name" => "test team",
+  "league" => league.id,
+  "user" => user.id,
+})
+
+Fowl.Leagues.create_team_player(%{
+  "team" => team.id,
+  "player" => last_player.id,
+})
